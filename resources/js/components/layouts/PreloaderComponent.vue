3bf278e7003651077bac18155667cfb7e1334089
Luiz Henrique Santos <luiz.santos85@gmail.com>
2022-10-23 22:38:35 -0300

    implementando preloader

---
+++ resources/js/components/layouts/PreloaderComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="preloader" v-show="loading">
+    <img src="/img/loading-buffering.gif" alt="preloader">
+  </div>
+</template>
+
+<script>
+export default {
+    computed:{
+        loading(){
+            return this.$store.state.preloader.loading;
+        }
+    }
+}
+</script>
+
+<style scoped>
+.preloader{
+    background: #000;
+    position: absolute;
+    z-index: 1;
+    opacity: .3;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    top: 0;
+    left: 0;
+    margin: 0;
+    padding:10% 0;
+}
+.preloader img{
+    max-width: 60px;
+}
+</style>
---
+++ resources/js/vuex/modules/preloder/preloder.js
@@ -0,0 +1,12 @@
+export default{
+    state: {
+        loading: false
+    },
+    mutations: {
+        CHANGE_PRELOADER(state, status) {
+            state.loading = status;
+        }
+    },
+    actions:{},
+    getters:{},
+}
